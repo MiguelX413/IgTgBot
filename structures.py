6b0e110d7ef7cf8f1d2bbc349aa560d1341c8986
MiguelX413 <lbpgaming27@gmail.com>
2022-04-19 01:01:06 -0700

    Reworked entity adding with the addition of Pair.append()

--- structures.py
+++ structures.py
@@ -2,7 +2,7 @@
 from typing import NamedTuple, List, Dict, Set, Optional
 
 from instaloader import Post
-from telegram import MessageEntity
+from telegram import MessageEntity, User
 
 emojis: Dict[str, str] = {
     "person": "👤",
@@ -58,9 +58,37 @@ def find_occurrences(string: str, substring: str) -> Set[int]:
     return offsets
 
 
-class Pair(NamedTuple):
-    caption: str
-    entities: List[MessageEntity]
+class Pair:
+    caption: str = ""
+    entities: List[MessageEntity] = []
+
+    def __init__(
+        self, caption: str = "", entities: Optional[List[MessageEntity]] = None
+    ):
+        self.caption = caption
+        if entities is None:
+            self.entities = []
+
+    def append(
+        self,
+        text: str,
+        type: Optional[str] = None,
+        url: Optional[str] = None,
+        user: Optional[User] = None,
+        language: Optional[str] = None,
+    ):
+        if type is not None:
+            self.entities.append(
+                MessageEntity(
+                    type=type,
+                    offset=utf16len(self.caption),
+                    length=utf16len(text),
+                    url=url,
+                    user=user,
+                    language=language,
+                )
+            )
+        self.caption += text
 
 
 class Pairs:
@@ -72,8 +100,7 @@ class Pairs:
     def long(self, counter: Optional[int] = None) -> Pair:
         """Create a Pair object from a given post"""
         # Initializing
-        caption: str = ""
-        entities: List[MessageEntity] = []
+        pair = Pair()
 
         # Media URL
         if counter is None:
@@ -87,99 +114,81 @@ class Pairs:
                 media_url = node.video_url
             else:
                 media_url = node.display_url
-        entities.append(
-            MessageEntity(
-                type="text_link",
-                offset=utf16len(caption),
-                length=utf16len("Media"),
-                url=media_url,
-            )
-        )
-        caption += "Media\n"
+        pair.append("Media", type="text_link", url=media_url)
+        pair.append("\n")
 
         # Posting account and Counter
-        entities.append(
-            MessageEntity(
-                type="text_link",
-                offset=utf16len(caption),
-                length=utf16len(f"@{self._post.owner_username}"),
-                url=f"https://instagram.com/{self._post.owner_username}/",
-            )
+        pair.append(
+            f"@{self._post.owner_username}",
+            type="text_link",
+            url=f"https://instagram.com/{self._post.owner_username}/",
         )
-        caption += f"@{self._post.owner_username} ({self._post.owner_id}): https://instagram.com/p/{self._post.shortcode}/"
+        pair.append(
+            f" ({self._post.owner_id}): https://instagram.com/p/{self._post.shortcode}/"
+        )
         if counter is not None:
-            caption += f" {counter + 1}/{self._post.mediacount}"
-        caption += "\n"
+            pair.append(f" {counter + 1}/{self._post.mediacount}")
+        pair.append("\n")
 
         # Title
         if self._post.title not in (None, ""):
-            caption += f"{self._post.title}\n"
+            pair.append(f"{self._post.title}\n")
 
         # Sponsor(s)
         if self._post.is_sponsored:
-            caption += "Sponsors:"
+            pair.append("Sponsors:")
             for sponsor_user in self._post.sponsor_users:
-                caption += " "
-                entities.append(
-                    MessageEntity(
-                        type="text_link",
-                        offset=utf16len(caption),
-                        length=utf16len(f"@{sponsor_user.username}"),
-                        url=f"https://instagram.com/{sponsor_user.username}/",
-                    )
+                pair.append(" ")
+                pair.append(
+                    f"@{sponsor_user.username}",
+                    type="text_link",
+                    url=f"https://instagram.com/{sponsor_user.username}/",
                 )
-                caption += f"@{sponsor_user.username} ({sponsor_user.userid})"
+                pair.append(f" ({sponsor_user.userid})")
 
-            caption += "\n"
+            pair.append("\n")
 
         # Tagged Users
         if len(self._post.tagged_users) > 0:
-            caption += emojis["person"]
+            pair.append(emojis["person"])
             for tagged_user in self._post.tagged_users:
-                caption += " "
-                entities.append(
-                    MessageEntity(
-                        type="text_link",
-                        offset=utf16len(caption),
-                        length=utf16len(f"@{tagged_user.username}"),
-                        url=f"https://instagram.com/{tagged_user.username}/",
-                    )
+                pair.append(" ")
+                pair.append(
+                    f"@{tagged_user.username}",
+                    type="text_link",
+                    url=f"https://instagram.com/{tagged_user.username}/",
                 )
-                caption += f"@{tagged_user.username} ({tagged_user.id})"
-            caption += "\n"
+                pair.append(f" ({tagged_user.id})")
+            pair.append("\n")
 
         # Location
         if self._post.location is not None:
-            entities.append(
-                MessageEntity(
-                    type="text_link",
-                    offset=utf16len(f"{caption}{emojis['location']}"),
-                    length=utf16len(self._post.location.name),
-                    url=f"https://instagram.com/explore/locations/{self._post.location.id}/",
-                )
+            pair.append(emojis["location"])
+            pair.append(
+                f"{self._post.location.name}",
+                type="text_link",
+                url=f"https://instagram.com/explore/locations/{self._post.location.id}/",
             )
-            caption += f"{emojis['location']}{self._post.location.name}\n"
+            pair.append("\n")
 
         # Views, Likes, and Comments
         if self._post.is_video:
-            caption += f"{emojis['eyes']}{self._post.video_view_count} "
-        entities.append(
-            MessageEntity(
-                type="text_link",
-                offset=utf16len(f"{caption}{emojis['heart']}"),
-                length=utf16len(str(self._post.likes)),
-                url=f"https://instagram.com/p/{self._post.shortcode}/liked_by/",
-            )
+            pair.append(f"{emojis['eyes']}{self._post.video_view_count} ")
+        pair.append(emojis["heart"])
+        pair.append(
+            f"{self._post.likes}",
+            type="text_link",
+            url=f"https://instagram.com/p/{self._post.shortcode}/liked_by/",
         )
-        caption += f"{emojis['heart']}{self._post.likes} {emojis['comments']}{self._post.comments}\n"
+        pair.append(f" {emojis['comments']}{self._post.comments}\n")
 
         # Date
-        caption += f"{emojis['calendar']}{self._post.date_utc:%Y-%m-%d %H:%M:%S}\n"
+        pair.append(f"{emojis['calendar']}{self._post.date_utc:%Y-%m-%d %H:%M:%S}\n")
 
         # Post Caption
         if self._post.caption is not None:
-            old_caption = caption
-            caption += self._post.caption
+            old_caption = pair.caption
+            pair.append(self._post.caption)
 
             # Mentions + Hashtags
             search_caption = (
@@ -195,10 +204,10 @@ class Pairs:
                     search_caption, f"@{caption_mention}"
                 ):
                     if mention_occurrence not in mention_occurrences:
-                        entities.append(
+                        pair.entities.append(
                             MessageEntity(
                                 type="text_link",
-                                offset=utf16len(caption[0:mention_occurrence]),
+                                offset=utf16len(pair.caption[0:mention_occurrence]),
                                 length=utf16len(f"@{caption_mention}"),
                                 url=f"https://instagram.com/{caption_mention}/",
                             )
@@ -214,27 +223,26 @@ class Pairs:
                     search_caption, f"#{caption_hashtag}"
                 ):
                     if hashtag_occurrence not in hashtag_occurrences:
-                        entities.append(
+                        pair.entities.append(
                             MessageEntity(
                                 type="text_link",
-                                offset=utf16len(caption[0:hashtag_occurrence]),
+                                offset=utf16len(pair.caption[0:hashtag_occurrence]),
                                 length=utf16len(f"#{caption_hashtag}"),
                                 url=f"https://instagram.com/explore/tags/{caption_hashtag}/",
                             )
                         )
                     hashtag_occurrences.add(hashtag_occurrence)
 
-        return Pair(caption, entities)
+        return pair
 
     def short(self, counter: Optional[int] = None) -> Pair:
-        short_caption: str
-        short_entities: List[MessageEntity] = []
+        pair = Pair()
         long = self.long(counter)
 
         if len(long.caption) > 1024:
-            short_caption = f"{long.caption[0:1023]}…"
+            pair.caption = f"{long.caption[0:1023]}…"
         else:
-            short_caption = long.caption
+            pair.caption = long.caption
 
         for long_entity in list(long.entities):
             if (
@@ -253,7 +261,7 @@ class Pairs:
                     )
                     < 1024
                 ):
-                    short_entities.append(
+                    pair.entities.append(
                         MessageEntity(
                             type=long_entity.type,
                             offset=long_entity.offset,
@@ -266,7 +274,7 @@ class Pairs:
                         )
                     )
             else:
-                short_entities.append(
+                pair.entities.append(
                     MessageEntity(
                         type=long_entity.type,
                         offset=long_entity.offset,
@@ -275,4 +283,4 @@ class Pairs:
                     )
                 )
 
-        return Pair(short_caption, short_entities)
+        return pair
